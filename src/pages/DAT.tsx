b346a07ee7021c44788e02f2f035746f3410bd65
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2025-12-25 14:14:24 +0100

    feat: add DAT minting page route
    - Create /dat page component
    - Integrate DATMinter component
    - Add navigation link in header

---
+++ src/pages/DAT.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/Header";
+import DATMinter from "@/components/DATMinter";
+import Footer from "@/components/Footer";
+
+const DAT = () => {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <DATMinter />
+      <Footer />
+    </div>
+  );
+};
+
+export default DAT;
+
